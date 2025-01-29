0fb6608dc463cee1897fb5801c40abb72be6a8cc
ankylosaurus11 <dylanjkubic@gmail.com>
2025-01-28 20:35:44 -0500

    create handle and fileserver for index.html

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("."))
 
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+
+	mux.Handle("/", fs)
 
 	fmt.Printf("Server starting on :%s...\n", port)
 	if err := server.ListenAndServe(); err != nil {
