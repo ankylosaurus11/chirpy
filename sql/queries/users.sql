23b37bfc73eb5542e28f801273505441de5589c4
Dylan Kubic <dylanjkubic@gmail.com>
2025-03-08 09:03:14 -0500

    create chirpy red migration and associated upgrade query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -20,3 +20,8 @@ UPDATE users
 SET updated_at = $1, email = $2, hashed_password = $3
 WHERE id = $4
 RETURNING id, updated_at, email;
+
+-- name: UpgradeRed :exec
+UPDATE users
+SET is_chirpy_red = $1
+WHERE id = $2;
